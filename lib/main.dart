d933a12c04fd0f25b7395876d041ae09cd535b46
Doaa Abdallah <122896725+DoaaAbdallah1@users.noreply.github.com>
2024-02-22 19:59:49 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
- 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,35 +16,70 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class SimpleProject extends StatelessWidget {
   const SimpleProject({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        elevation: 50,
-        title: Text("Notes"),
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        elevation: 0,
+        title: Text(
+          "Notes",
+          style: TextStyle(
+            color: Color.fromARGB(255, 243, 130, 130),
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
         centerTitle: true,
-        leading: IconButton(onPressed: () {  },icon:Icon(Icons.share,size: 28,),),
-         
-        actions: [IconButton(onPressed: () {  },icon:Icon(Icons.search,size: 23,),),
-        IconButton(onPressed: () {  },icon:Icon(Icons.delete,size: 23,)),],
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.share,
+            size: 26,
+            color: Color.fromARGB(255, 243, 130, 130),
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              size: 23,
+              color: Color.fromARGB(255, 243, 130, 130),
+            ),
+          ),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.delete,
+                size: 23,
+                color: Color.fromARGB(255, 243, 130, 130),
+              )),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: SizedBox(
-          child: Text(
-            "Mathematical Notes is a peer-reviewed journal featuring research papers and review articles in various fields of theoretical mathematics.😇\n\n\n1.Includes pure and applied mathematics, statistics and probability, and discrete mathematics.🤓\n\n2.Includes geometry, calculus, logic, number theory, and mathematical physics.🤓\n\n3.Welcomes manuscripts from all countries.🤓",
-            style: TextStyle(
-              fontSize: 18 ,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'Oswald',
-              
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Mathematical Notes is a peer-reviewed journal featuring research papers and review articles in various fields of theoretical mathematics.😇\n\n\n1.Includes pure and applied mathematics, statistics and probability, and discrete mathematics.🤓\n\n2.Includes geometry, calculus, logic, number theory, and mathematical physics.🤓\n\n3.Welcomes manuscripts from all countries.🤓",
+              style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
             ),
           ),
         ),
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.keyboard_arrow_right, color: Colors.white),
+        backgroundColor: Color.fromARGB(255, 243, 130, 130),
+      ),
     );
   }
 }
