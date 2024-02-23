7dddd880028d8a674addf09b7849d8bffddf5a86
Doaa Abdallah <122896725+DoaaAbdallah1@users.noreply.github.com>
2024-02-23 15:42:18 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -62,19 +63,44 @@ class SimpleProject extends StatelessWidget {
               )),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SizedBox(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
+      body: Container(
+        child:Text(
               "Mathematical Notes is a peer-reviewed journal featuring research papers and review articles in various fields of theoretical mathematics.😇\n\n\n1.Includes pure and applied mathematics, statistics and probability, and discrete mathematics.🤓\n\n2.Includes geometry, calculus, logic, number theory, and mathematical physics.🤓\n\n3.Welcomes manuscripts from all countries.🤓",
               style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
             ),
+          
+          // color: Color.fromARGB(255, 148, 207, 255),
+          margin: EdgeInsets.all(50),
+          padding: EdgeInsets.all(10),
+          height: 523,
+          width: 300,
+          // alignment: Alignment.center,
+          // transform: Matrix4.rotationY(.07),
+          decoration: BoxDecoration(
+               color: Color.fromARGB(255, 148, 207, 255),
+               border: Border.all(color: Color.fromARGB(255, 243, 130, 130),width: 2.4),
+               borderRadius: BorderRadius.circular(10),
+               boxShadow: [BoxShadow(
+                color: Colors.amber.shade100,
+                blurRadius: 8,
+                offset: Offset(3.0, 3.0),
+               spreadRadius: 10.0,
+
+               ),
+               BoxShadow(
+                color: Colors.amber.shade100,
+                blurRadius: 8,
+                offset: Offset(5.0, 5.0),
+                spreadRadius: 10.0,
+               
+               ),
+              
+               ],
           ),
-        ),
+
       ),
+          
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(Icons.keyboard_arrow_right, color: Colors.white),
